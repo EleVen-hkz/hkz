f617e8071398c9b9283d9049a94f8c255709b1da
EleVen <951291318@qq.com>
2020-11-19 16:31:22 +0800

    update

--- Python/Lottery.py
+++ Python/Lottery.py
@@ -1,21 +1,25 @@
 #双色球模拟器
 #6个红色球号码和1个蓝色球号码组成。红色球号码从1--33中选择；蓝色球号码从1--16中选择
-import random
+import random,time
 myred=[2,6,14,16,19,24]
 mybule=[14]
 count=0
 while 1 :
+    time.sleep(0.3)
     red=[]
-    for i in range(0,6):
-        red.append(random.randint(1,33))
+    while len(red) < 6:
+        n=random.randint(1,33)
+        if n not in red:
+            red.append(n)
     bule=random.randint(1,16)
     red.sort()
+    print(red,bule)
     if myred == red :
         if mybule == bule:
             print("WOW,500万,一共%s次"% count)
             break
         else:
-            print("红球都对了，可惜就差一点。已经玩了%s次"% count)
+            print("就差一点,已经%s次"% count)
             count = count + 1
     else:
         count=count+1
